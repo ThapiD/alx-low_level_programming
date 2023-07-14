92ddc01c8d831688e45200038c12cef033e48072
ThapiD <mmathapelodavids@icloud.com>
2023-07-14 14:25:07 -0700

    complete source code to print whether number stored in variable in negative or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,17 +10,16 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n = 98;
+	int n = RAND_MAX;
 	{
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	scanf("%d", n);
-
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else ((n == 0))
+	else ((n = 0))
 		printf("%d is zero\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
