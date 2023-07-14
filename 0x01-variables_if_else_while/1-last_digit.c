6245cd3d519f4257c25efd7677919acd7a80cbd7
ThapiD <mmathapelodavids@icloud.com>
2023-07-14 16:02:19 -0700

    source code that prints alst digit of the number stored in a variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,24 +10,24 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	char is;
+	char lastDigit;
 	int n;
 
-	is = "Last digit of";
+	lastDigit = "Last digit of";
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 5)
 	{
-		printf("%s %d and is greater than 5\n", is, n);
+		printf("%s %d and is greater than 5\n", lastDigit, n);
 	}
 	else if (n == 0)
 	{
-		printf("%s %d and is 0\n", is, n);
+		printf("%s %d and is 0\n", lastDigit, n);
 	}
 	else
 	{
-		printf("%s %d and is less than 6 and not 0\n", is, n);
+		printf("%s %d and is less than 6 and not 0\n", lastDigit, n);
 	}
 	return (0);
 }
