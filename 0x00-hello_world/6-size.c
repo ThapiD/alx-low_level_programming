79dea918fdcfe23dfb4918fa93c69c7438ff186d
ThapiD <mmathapelodavids@icloud.com>
2023-07-13 18:02:43 -0700

    sizes of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(sucess)
+ */
+
+int main(void)
+{
+	int integerType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %ld bytes\n",sizeof(integerType));
+	printf("Size of float: %ld bytes\n",sizeof(floatType));
+	printf("Size of double: %ld bytes\n",sizeof(doubleType));
+	printf("Size of char: %ld bytes\n",sizeof(charType));
+	return (0);
+}
