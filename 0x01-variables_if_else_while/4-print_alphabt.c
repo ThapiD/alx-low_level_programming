8b97f4e75833baefdce2572939a8488aace9045d
ThapiD <mmathapelodavids@icloud.com>
2023-07-14 18:59:37 -0700

    lowercase alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0(success)
+ *
+ */
+int main(void)
+{
+	char alphabet[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
