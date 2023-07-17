b14450bd2e52053a3f7e719d07d82f40837c1e74
ThapiD <mmathapelodavids@icloud.com>
2023-07-17 16:01:17 -0700

    single digit numbers of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,11 +6,12 @@
  */
 int main(void)
 {
-	int i = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33";
+	char singleDigit[25] = "0 1 2 3 4 5 6 7 8 9 11 22 33";
+	int i;
 
 	for (i = 0; i < 40; i++)
 	{
-		printf("%d", i)
+		putchar("singleDigit[i]");
 	}
 	putchar('\n');
 		return (0);
