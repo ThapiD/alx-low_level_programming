fc84dccf23f7f18741234198bda2bfcab519b26c
ThapiD <mmathapelodavids@icloud.com>
2023-07-17 15:50:53 -0700

    single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int i = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33";
+
+	for (i = 0; i < 40; i++)
+	{
+		printf("%d", i)
+	}
+	putchar('\n');
+		return (0);
+}
