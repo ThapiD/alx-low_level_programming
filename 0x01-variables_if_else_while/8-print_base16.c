f368e90acc741cf89ab16d8813ae92b0a309a107
ThapiD <mmathapelodavids@icloud.com>
2023-07-17 17:30:32 -0700

    base 16 numbers with lowercase alphabets

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,11 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-		putchar('alphabet');
+		
+		if (i < 9)
+		{	
+			putchar('alphabet');
+		}
 	}
 	putchar('\n');
 		return (0);
