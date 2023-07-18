6b31b18c5eacf081ddb54ccc270f1f9c2f540531
ThapiD <mmathapelodavids@icloud.com>
2023-07-17 19:01:17 -0700

    single digits in ascending order

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i + '0');
+		putchar((i % 10) + '0');
 	}
-		if (i < 9)
+		if (i == 9)
 		{
 			putchar(",");
 			putchar(' ');
