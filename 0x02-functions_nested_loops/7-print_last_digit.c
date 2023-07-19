c3864be5de8d90f409d85e70a12d9e7327c22f8c
ThapiD <mmathapelodavids@icloud.com>
2023-07-18 19:14:28 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,15 @@
 int print_last_digit(int i)
 {
 	int j;
+
+	if (i < 0)
+	i = -i;
+
 	j = i % 10;
 
 	if (j < 0)
-	{
-		_putchar(-j + 0);
-		return (-j);
-	}
-	else
-	{
-		_putchar(j +0);
-		return (+i);
-	}
+		j = -j;
+
+	_putchar(j + '0');
+	return (j);
 }
