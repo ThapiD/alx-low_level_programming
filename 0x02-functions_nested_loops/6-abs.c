7f940ef565906835bda1ff61b065cab238078118
ThapiD <mmathapelodavids@icloud.com>
2023-07-18 18:43:11 -0700

    computes absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @x: absolute integer value
+ *
+ * Return: value of integer
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
