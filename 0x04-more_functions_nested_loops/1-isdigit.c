8c3b29f51a297169785a9550a14172a057297510
ThapiD <mmathapelodavids@icloud.com>
2023-07-20 09:19:43 -0700

    checks for a digit from 0-9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,12 @@
 /**
  * _isdigit - checks for a digit
  * @c: check for digit
+ *
  * Return: 1 for digit and 0 for otherwise
  */
 int _isdigit(int c)
 {
-	if ((c >= '0' && c < '9'))
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
