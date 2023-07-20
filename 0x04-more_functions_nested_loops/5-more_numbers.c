a4cac24f20766a3abc00a51f8150a15d58f22fb2
ThapiD <mmathapelodavids@icloud.com>
2023-07-20 10:55:29 -0700

    draws a straight line

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - Prints the numbers from 0 - 14, 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	char i, j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			for (i >= 10)
+				_putchar(1);
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - draws straight line in terminal
+ * @n: check for numbers of times character is printed
+ * 
+ * Return: 0(success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+			_putchar('_');
+	}
+	_putchar('\n');
+}
