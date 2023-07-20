f2fc283a4750bef2238c37dbdcd14d2c71895c07
ThapiD <mmathapelodavids@icloud.com>
2023-07-20 06:52:46 -0700

    checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: check for digit
+ * Return: 1 for digit and 0 for otherwise
+ */
+int _isdigit(int c)
+{
+	for (c = 0; c < 10; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
