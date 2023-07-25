087de11d0dc4cf0893fc90c24c9f9ec5600b83de
ThapiD <mmathapelodavids@icloud.com>
2023-07-25 15:29:36 -0700

    length of s string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: string to be counted
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int i;
+	int num;
+
+	for (i = 0; s[i] != '\0'; i++)
+		num++;
+	return (num);
+}
