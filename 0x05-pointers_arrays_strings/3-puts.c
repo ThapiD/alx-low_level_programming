2151ef44f506b1a544673624595ef73fe1bbb803
ThapiD <mmathapelodavids@icloud.com>
2023-07-25 16:51:27 -0700

    prints strings in reverse

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 /**
  * _puts - prints a string to stdout
  * @str: string to be printed
- * Return: void
+ *
  */
 void _puts(char *str)
 {
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints strings in revers
+ * @s: for string
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+	int num = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		num++;
+	for (i = num - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
