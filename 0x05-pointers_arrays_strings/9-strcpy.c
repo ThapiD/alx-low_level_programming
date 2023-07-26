54c30936626297f0b2c098715b78eaf484b1be1c
ThapiD <mmathapelodavids@icloud.com>
2023-07-25 18:29:24 -0700

    copies string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,13 +4,16 @@
  * _strcpy - copies string
  * @dest: destination
  * @src: source
+ * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[1] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
-	dest[i] = '\0';
+	}
+	dest[i++] = '\0';
 	return (dest);
 }
