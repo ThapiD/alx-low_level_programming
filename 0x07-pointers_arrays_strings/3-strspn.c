c1f83bf1dff28c8e34c12a4c0e936f14a5fab236
ThapiD <mmathapelodavids@icloud.com>
2023-08-01 17:58:02 -0700

    Function that gets thr prefix of a substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,16 +3,17 @@
 /**
  * _strspn: Function that gets lenght of a prefix substring
  * @s: string to check from
- * @accepts: string to be checked
- * Return: charcters checked
+ * @accept: string to be checked
+ * Return: number charcters checked
  *
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	int a, b;
 
-	for (a = 0; a[s] != 0; a++)
-		for (b = 0; b[s] != accept(b); b++)
+	for (a = 0; s[a] != 0; a++)
+		for (b = 0; s[b] != accept(b); b++)
 			if (accept(b) == '\0')
+				return (a);
 	return (0);
 }
