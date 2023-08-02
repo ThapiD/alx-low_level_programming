1beb3cf82afa2d684ce5edd7cef3e1867d502a57
ThapiD <mmathapelodavids@icloud.com>
2023-08-01 18:11:10 -0700

    Function that gets the prefix of a substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,9 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int a, b;
 
-	for (a = 0; s[a] != 0; a++)
-		for (b = 0; s[b] != accept(b); b++)
-			if (accept(b) == '\0')
+	for (a = 0; s[a] != '\0'; a++)
+		for (b = 0; s[a] != accept[b]; b++)
+			if (accept[b] == '\0')
 				return (a);
 	return (0);
 }
