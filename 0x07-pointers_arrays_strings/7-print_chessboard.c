84b8cd4b11363802552ae183843e9473c48ba8fd
ThapiD <mmathapelodavids@icloud.com>
2023-08-01 20:12:57 -0700

    function that prints a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Function that prints chessboard
+ * @a: for row of chessboard
+ * Return: 0 always
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b, n;
+	char k;
+
+	for (a = 0; a <= n; a++)
+		for (b = 0; b <= 8; b++)
+			if (k == n)
+}
