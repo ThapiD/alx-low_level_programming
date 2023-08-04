ba819a251c1503f69771bd2c87f65817cc519fbf
ThapiD <mmathapelodavids@icloud.com>
2023-08-03 19:37:39 -0700

    Fucntion that prints length of string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 1)
+	if (*s != '\0')
 	{
-		return;
-	_puts_recursion(s);
-	_putchar(*s);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	else 
+	if (*s == '\0')
+	{
 		_putchar('\n');
+	}
 }
