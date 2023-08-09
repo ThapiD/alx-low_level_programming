a35173854e90ab01db032feb056a39018f379297
ThapiD <mmathapelodavids@icloud.com>
2023-08-09 16:42:42 -0700

    Program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
 /**
-<<<<<<< HEAD
- * main - Prints all arguments
- * @argc: number of comman lines
- * @argv: array of command lines
-=======
  * main - Program that prints all arguments it receives
  * @argc: number of command lines
  * @argv: arrays in command lines
->>>>>>> d789045707041e255358432e30a2977c8a6fb5aa
  * Return: 0 always success
  */
 int main(int argc, char *argv[])
@@ -18,11 +12,7 @@ int main(int argc, char *argv[])
 
 	for (n = 0; n < argc; n++)
 	{
-<<<<<<< HEAD
-		printf("%s\n", argv[n]);;
-=======
-		printf("%s\n", argv[v]);
->>>>>>> d789045707041e255358432e30a2977c8a6fb5aa
+		printf("%s\n", argv[n]);
 	}
 	return (0);
 }
