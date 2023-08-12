8caa9561331ec5d50cef01f6480500ccd613e3ea
ThapiD <mmathapelodavids@icloud.com>
2023-08-11 18:33:33 -0700

    Function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,40 +15,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int a = 0, i = 0, space1 = 0, space2 = 0;
 
 	while (s1 && s1[space1])
-	{
 		space1++;
-	}
 	while (s2 && s2[space2])
-	{
 		space2++;
-	}
 
 	if (n < space2)
-	{
 		p = malloc(sizeof(char) * (space1 + n + 1));
-	}
 	else
-	{
 		p = malloc(sizeof(char) * (space1 + space2 + 1));
-	}
 
 	if (!p)
-	{
 		return (NULL);
-	}
-	while (i < space1)
+
+	while (a < space1)
 	{
 		p[a] = s1[a];
 		a++;
 	}
 	while (n < space2 && a < (space1 + n))
-	{
 		p[a++] = s2[i++];
-	}
+
 	while (n >= space2 && a < (space1 + space2))
-	{
 		p[a++] = s2[i++];
-	}
+
 	p[a] = '\0';
+
 	return (p);
 }
