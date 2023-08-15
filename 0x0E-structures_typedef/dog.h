5f9775c5262c10f2c57d00f3b8db3b28dfd1deaa
ThapiD <mmathapelodavids@icloud.com>
2023-08-15 20:24:06 +0200

    Defining a new new name for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,8 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} a_dog;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 
 #endif
