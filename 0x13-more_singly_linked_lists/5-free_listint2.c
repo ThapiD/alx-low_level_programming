c91684be5731baab3c4b5920640015c48f04fe12
ThapiD <mmathapelodavids@icloud.com>
2023-08-29 23:14:42 +0200

    Function that frees list with double pointer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,12 @@
  */
 void free_listint2(listint_t **head)
 {
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		listint_t *temp = *head;
 		*head = (*head)->next;
-		free(temp);
+		free(temp);;
 	}
-	head = NULL;
 }
