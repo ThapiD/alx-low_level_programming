dc905af1a6d2d488ff94138992975ceb885c59b4
ThapiD <mmathapelodavids@icloud.com>
2023-08-30 03:10:14 +0200

    Function that returns the nth node of a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- * push - Function to push new node to front of list
- * @head: pointer to pointer of head
- * @new_n: new data for new node
- * Return: void
- */
-void push(listint_t **head, unsigned int new_n)
-{
-	listint_t *newnode;
-
-	newnode = malloc(sizeof(listint_t));
-
-	newnode->n = new_n;
-	newnode->next = (*head);
-	(*head) = newnode;
-}
-
-/**
  * get_nodeint_at_index - Function that returns the nth node
  * a linked list
  * @head: head pointer
