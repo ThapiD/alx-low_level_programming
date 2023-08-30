6b849037347bf444242db276c96f744f65c8ef50
ThapiD <mmathapelodavids@icloud.com>
2023-08-30 03:12:22 +0200

    Function that returns the sum of all the data (n) of a linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
- * push - Function that inserts a new node at
- * beginning of linked list
- * @head: pointer to pointer to head
- * @new_n: new data for new node
- * Return: void
- */
-void push(listint_t **head, int new_n)
-{
-	listint_t *newnode;
-
-	newnode = malloc(sizeof(listint_t));
-
-	newnode->n = new_n;
-	newnode->next = (*head);
-	(*head) = newnode;
-}
-
-/**
  * sum_listint - Function that returns the sum of all the data
  * of a linked list
  * @head: poinetr to pointer to head
