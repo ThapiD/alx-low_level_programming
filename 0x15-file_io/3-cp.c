d7fa4b20111be6007d4f7ef620f5baadd27608a5
ThapiD <mmathapelodavids@icloud.com>
2023-09-06 03:54:28 +0200

    Program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,17 +2,6 @@
 #define BUFFER_SIZE 1024
 
 /**
- * error_and_exit - Function tht prints a error message and exits the program
- * @exits: exit code
- * @text: error text to be printed
- * Return: void
- */
-void error_and_exit(int exits, const char *text)
-{
-	dprintf(STDERR_FILENO, "%s\n", text);
-	exit(exits);
-}
-/**
  * main - Program that copies the content of one file to the other
  * @argc: number of arguments
  * @argv: array of arguments
@@ -20,26 +9,20 @@ void error_and_exit(int exits, const char *text)
  */
 int main(int argc, char *argv[])
 {
-	const char *ff, *ft;
+	const char *ff = argc[1], *ft = argv[2];
 	int i, j;
 	ssize_t rb, wb;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-	{
 		error_and_exit(97, "Usage: cp file_from file_to");
-	}
-
-	ff = argv[1];
-	ft = argv[2];
 
 	i = open(ff, O_RDONLY);
 	if (i == -1)
 	{
 		error_and_exit(98, "Error: Can't read that file");
 	}
-
-	j = open(ft, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	j = open(ft, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (j == -1)
 	{
 		close(i);
@@ -49,11 +32,9 @@ int main(int argc, char *argv[])
 	{
 		wb = write(j, buffer, rb);
 		if (wb == -1)
-		{
 			close(i);
 			close(j);
 			error_and_exit(99, "Error: Can't write to file");
-		}
 	}
 	if (rb == -1)
 	{
@@ -62,8 +43,6 @@ int main(int argc, char *argv[])
 		error_and_exit(98, "Error: Can't read that file");
 	}
 	if (close(i) == -1 || close(j) == -1)
-	{
 		error_and_exit(100, "Error: Can't close this description");
-	}
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,17 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+ * error_and_exit - Function tht prints a error message and exits the program
+ * @exits: exit code
+ * @text: error text to be printed
+ * Return: void
+ */
+void error_and_exit(int exits, const char *text)
+{
+        dprintf(STDERR_FILENO, "%s\n", text);
+        exit(exits);
+}
 void print_error_and_exit(int exit_code, const char *message);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
